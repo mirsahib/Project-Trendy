fb7b121ed9aa0b8f12c10f3f7e1f16f1da6c3c38
mirsahib <mirsahib24@gmail.com>
2022-06-03 19:00:01 +0600

    :wrench: Add docker configuration

--- server/index.ts
+++ server/index.ts
@@ -8,13 +8,13 @@ const server = async()=>{
     const app:Express = express()
     
     await expressApp(app)
-
-    //database connection
-    await connection()
+ 
+    //database connection  
+    //await connection()
 
     app.listen(3000,()=>{
         console.log('Server is running at',3000)
     })
-}
+} 
 
 server()
