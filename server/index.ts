8f7ca468a4ae576660f8641c2b2f3eaee2371385
mirsahib <mirsahib24@gmail.com>
2022-06-06 11:54:53 +0600

    :wrench: Define app port

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,13 @@
 import express,{Express} from 'express'
 import expressApp from './express'
-import connection from './database/connection'
-
-
 
 const server = async()=>{
     const app:Express = express()
     
     await expressApp(app)
- 
-    //database connection  
-    await connection()
 
-    app.listen(3000,()=>{
-        console.log('Server is running at',3000)
+    app.listen(process.env.TRENDY_PORT||8080,()=>{
+        console.log('Server is running at',process.env.TRENDY_PORT)
     })
 } 
 
