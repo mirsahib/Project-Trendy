67f3ba24e99a78a3c18b0fad3d831ec297e91638
mirsahib <mirsahib24@gmail.com>
2022-06-14 19:20:05 +0600

    :hammer: Add jwt token util function

--- server/util/utils.ts
+++ server/util/utils.ts
@@ -1,7 +1,7 @@
 
 import jwt from 'jsonwebtoken'
 const generateToken =async (payload:string) => {
-    return await jwt.sign(payload,'APP_SECRET',{expiresIn:'1d'})
+    return await jwt.sign({id:payload},'APP_SECRET',{expiresIn:60*60})
 }
 
 export default generateToken
