b49cad3de4771592414111fa1484a804b89abcc9
mirsahib <mirsahib24@gmail.com>
2022-06-14 22:24:59 +0600

    :package: Add cors

--- server/express.ts
+++ server/express.ts
@@ -1,4 +1,5 @@
 import express,{Express} from 'express'
+import cors from 'cors'
 import UserApi from './api/user'
 import ProductApi from './api/product'
 import OrderApi from './api/order'
@@ -6,6 +7,7 @@ import AuthApi from './api/auth'
 
 const app = async (app:Express)=>{
     app.use(express.json())
+    app.use(cors())
     
 
     //app router
