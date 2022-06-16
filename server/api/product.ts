40779a6cd857e6d9c567141d7d1b67b3df8a97ce
mirsahib <mirsahib24@gmail.com>
2022-06-16 21:10:24 +0600

    :hammer: update product api and controller

--- server/api/product.ts
+++ server/api/product.ts
@@ -1,23 +1,30 @@
 import { Express} from "express"
 import ProductService from "../service/product.service"
+import storage from "../util/cloudinary"
+import multer from 'multer'
 
 class ProductApi{
 
     private app:Express
     private ProductService:ProductService
+    private upload:multer.Multer
 
     constructor(app:Express){
         this.app = app
         this.ProductService = new ProductService
+        this.upload = multer({storage:storage})
 
         //register routes
         this.app.get('/api/product',this.ProductService.readAll)
-        this.app.post('/api/product',this.ProductService.create)
+        this.app.post('/api/product',this.upload.single('image'),this.ProductService.create)
         this.app.get('/api/product:id',this.ProductService.readByID)
         this.app.put('/api/product:id',this.ProductService.updateById)
         this.app.delete('/api/product:id',this.ProductService.deleteById)
 
     }
+
+    
+    
 }
 
 
--- server/service/product.service.ts
+++ server/service/product.service.ts
@@ -1,5 +1,7 @@
-import {  Request, Response } from "express"
-import ProductRepository, { IProduct } from "../database/repository/product.repository"
+import {  NextFunction, Request, Response } from "express"
+import ProductRepository from "../database/repository/product.repository"
+import ProductModel from '../database/model/Product.model'
+import errorMessage from '../util/error-handler'
 
 
 class ProductService {
@@ -11,33 +13,35 @@ class ProductService {
 
     create = async (req: Request, res: Response) => {
         try {
-            const { firstName, lastName }: IProduct = req.body
-            const user = await this.ProductRepository.createProduct({ firstName, lastName })
-            res.json({ 'user': user })
+            const item: ProductModel = req.body
+            if(req.file?.path){
+                item.image = req.file.path
+            }
+            const product  = await this.ProductRepository.createProduct(item)
+            res.json({ message: 'saved successfully',product:product})
         } catch (error) {
-            console.log('Product saved failed', error)
-            res.json({ "message": error })
+            errorMessage(error,res)
         }
     }
     readAll = async (req: Request, res: Response) => {
         try {
-            const users = await this.ProductRepository.read()
-            res.json({ 'Products': users })
+            const products = await this.ProductRepository.read()
+            res.json({ 'Products': products })
         } catch (error) {
-            res.json({ "error": error })
+            errorMessage(error,res)
         }
     }
     readByID=async(req: Request, res: Response) =>{
         try {
             const id = req.params.id
-            const user = await this.ProductRepository.readById(id)
-            if(user!=null){
-                res.json({"user":user})
+            const product = await this.ProductRepository.readById(id)
+            if(product!=null){
+                res.json({"product":product})
             }else{
-                res.json({"message":"user not found"})
+                res.json({"message":"product not found"})
             }
         } catch (error) {
-            res.json({"error":error})
+            errorMessage(error,res)
         }
 
     }
@@ -46,12 +50,12 @@ class ProductService {
             const id = req.params.id
             const updatedProduct = await this.ProductRepository.updateById(id,req.body)
             if(updatedProduct!=null){
-                res.json({"message":"user updated"})
+                res.json({"message":"product updated"})
             }else{
-                res.json({"message":"user not found"})
+                res.json({"message":"product not found"})
             }
         } catch (error) {
-            res.json({"error":error})
+            errorMessage(error,res)
         }
     }
     deleteById=async (req: Request, res: Response) =>{
@@ -59,13 +63,19 @@ class ProductService {
             const id = req.params.id
             const deletedProduct = await this.ProductRepository.deleteById(id)
             if(deletedProduct!=null){
-                res.json({"message":"user is deleted"})
+                res.json({"message":"product is deleted"})
             }else{
-                res.json({"message":"user not found"})
+                res.json({"message":"product not found"})
             }
         } catch (error) {
-            res.json({"error":error})
+            errorMessage(error,res)
         }
+    }
+
+    uploadImage=(req:Request,res:Response,next:NextFunction)=>{
+        console.log('middleware')
+        console.log(req.file?.path)
+        next()
     }
 
 }
